e7090bf5a029d09d07920034d3821a8fbe538473
reincas <r.caspary@posteo.de>
2024-04-16 15:02:14 +0200

    Add list of points to plane detection results

--- nanofactorysystem/tools/plane.py
+++ nanofactorysystem/tools/plane.py
@@ -224,6 +224,7 @@ class Plane(Parameter):
                 "gradient": plane.slope,
                 "polarAngle": plane.theta,
                 "azimuthAngle": plane.phi,
+                "points": points.tolist(),
                 }
         
         self.steps = []
